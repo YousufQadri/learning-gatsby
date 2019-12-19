a98241fc749d47cc5bae7720f409641c65d482e1
YousufQadri <s.yousuf@live.com>
2019-12-19 13:08:46 +0500

    Add: dynamic data from sitemetadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,20 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  console.log(data.site.siteMetadata.title)
+  return (
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+    </div>
+  )
+}

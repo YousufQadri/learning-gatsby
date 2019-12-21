50202b89026b89ddb4d417b082344b400889a121
YousufQadri <s.yousuf@live.com>
2019-12-21 09:48:50 +0500

    Add: header and footer component

---
+++ src/components/header.js
@@ -0,0 +1,64 @@
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import headerStyles from "./header.module.scss"
+const Header = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+  return (
+    <header className={headerStyles.header}>
+      <Link className={headerStyles.title} to="/">
+        {" "}
+        <h1>{data.site.siteMetadata.title}</h1>
+      </Link>
+      <nav>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.activeNavItem}
+              to="/blog"
+            >
+              Blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header

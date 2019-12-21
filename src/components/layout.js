c6a07c41398988b36475966367d0826ab3a196a9
YousufQadri <s.yousuf@live.com>
2019-12-21 09:48:29 +0500

    Add: Static layout for all pages

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./header"
+import Footer from "./footer"
+import LayoutStyles from "./layout.module.scss"
+const Layout = props => {
+  return (
+    <div className={LayoutStyles.container}>
+      <div className={LayoutStyles.content}>
+        <Header />
+        {props.children}
+      </div>
+      <Footer />
+    </div>
+  )
+}
+export default Layout
